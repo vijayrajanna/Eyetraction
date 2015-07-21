b709c8eadcc0e3023122ec273e14d707f1b8131e
vijayrajanna <vijay.drajanna@gmail.com>
2015-07-21 06:08:04 -0400

    simplified program

--- Adruino/Clicker/Clicker.ino
+++ Adruino/Clicker/Clicker.ino
@@ -4,31 +4,19 @@
 #define BAUDRATE 9600
 
 // Analog read 0 is pin 38 
-
 int volts = 0;
 int FSR_PIN = 0;
 int LED = 17;
-char Msg[2] ;
-char toPrint ;
+
+char toPrint = '\0';
 bool sent = false;
-char buffer[2] = {'O', 'K'};
+char state = ' ';
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
   Serial.begin(BAUDRATE);
   bluetooth.begin(BAUDRATE);
-
-  /*pinMode(LED, OUTPUT);
-  Serial.begin(9600);  // Begin the serial monitor at 9600bps
-  bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
-  bluetooth.print("$");  // Print three times individually
-  bluetooth.print("$");
-  bluetooth.print("$");  // Enter command mode
-  delay(100);  // Short delay, wait for the Mate to send back CMD
-  bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
-  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
-  bluetooth.begin(9600);  // Start bluetooth serial at 9600*/
 }
 
 void loop() {
@@ -40,44 +28,23 @@ void loop() {
   {
       //Serial.println("HIGH");
       digitalWrite(LED, HIGH);
-
-      bluetooth.write(buffer);
+      bluetooth.write(state);
       bluetooth.flush();
       sent = true;
-      delay(400);
       digitalWrite(LED, LOW);
   }
   else
   {
-      //bluetooth.write(buffer);
-      //bluetooth.flush();  
       delay(100);
   }
 
   ///////////////////////////////////////////////////////////
-  if(bluetooth.available())  // If the bluetooth sent any characters
+  if((bluetooth.available()) && (sent == true))
   {
-    // Send any characters the bluetooth prints to the serial monitor
-    int i = 0;
-    while(bluetooth.available() && (sent == true))
-    {
-	toPrint = (char)bluetooth.read();
-        Serial.print((char)toPrint);
-        i++;
-        delay(200);
-        if(i > 1) 
-        {
-          sent = false;
-          break;
-        }
-    }
-  }
-  
-  ////////////////////////////////////////////////////////////////
-  if(Serial.available())  // If stuff was typed in the serial monitor
-  {
-    // Send any characters the Serial monitor prints to the bluetooth
-    bluetooth.write((char)Serial.read());
-    bluetooth.flush();
-  }
+        Serial.println("printing : ");
+        toPrint = (char)bluetooth.read();
+        Serial.println((char)toPrint);
+        sent = false;
+   }
 }
+ 
